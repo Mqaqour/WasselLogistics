834047a802f9cecfc70d4fb3390fff2a376268d6
oday-louy <oziq@wassel.ps>
2026-06-02 14:11:54 +0300

    fix: replace Tailwind CDN with build-time compilation (postcss + v4)

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './index.css';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { App } from './App';
 import RegisterNewAppWestBank from './pages/RegisterNewAppWestBank';
